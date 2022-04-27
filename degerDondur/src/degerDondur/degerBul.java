5680a3f17bcd40250d9f5342851c22e05fef510c
Bulut <102280606+BulutMerve@users.noreply.github.com>
2022-04-27 18:20:52 +0300

    Gelen objenin türüne göre değeri yazdırıldı

--- degerDondur/src/degerDondur/degerBul.java
+++ degerDondur/src/degerDondur/degerBul.java
@@ -6,34 +6,59 @@ public class degerBul {
 
 	public static void main(String[] args)
 	{
-		//String yazdýrma
+		//String yazdÃ½rma
 		String ogrenciAdi = "Emre Bulut";
-		System.out.println("(Ogrenci Adý Yazdýrýlýyor...");
-		getValue(ogrenciAdi);
+		getValue(ogrenciAdi, "ad");
 		
-		//Integer yazdýrma
+		//Integer yazdÃ½rma
 		Integer yas = 17;
-		System.out.println("Yaþ Yazdýrýlýyor...");
-		getValue(yas);
+		getValue(yas, "yas");
 		
-		//Ogrenci nesnesiyle oluþturulan diziyi yazdýrma
-		Ogrenci yeniOgrenci = new Ogrenci(ogrenciAdi, yas);
+		//Ogrenci nesnesiyle oluÃ¾turulan diziyi yazdÃ½rma
+		Ogrenci yeniOgrenci = new Ogrenci("Emre", 15);
+		Ogrenci yeniOgrenci2 = new Ogrenci("Kerem", 22);
 		ArrayList<Ogrenci> ogrenciList = new ArrayList<>();
 		ogrenciList.add(yeniOgrenci);
-		System.out.println("Öðrenci Listesi (Nesne) Yazdýrýlýyor.");
-		getValue(ogrenciList);
+		ogrenciList.add(yeniOgrenci2);
+		getValue(ogrenciList, "ogrenciListNesne");
 	
-		// String dizisi yazdýrma
+		// String dizisi yazdÃ½rma
 		ArrayList<String> ogrenciList2 = new ArrayList<>();
 		ogrenciList2.add("Emre");
 		ogrenciList2.add("Kerem");
-		System.out.println("Öðrenci Listesi(String) Yazdýrýlýyor.");
-		getValue(ogrenciList2);
+		getValue(ogrenciList2, "ogrenciListString");
 	}
 	
-	public static void getValue(Object object)
+	public static void getValue(Object object, String turu)
 	{
-		System.out.println("Gelen deðer= "+ object);
+		switch(turu) {
+		  case "ad":
+			  System.out.println("Ogrenci Adi Yazdiriliyor..");
+			  System.out.println(object);
+		    break;
+		  case "yas":
+			  System.out.println("Ogrenci Yasi Yazdiriliyor.");
+			  System.out.println(object);
+		    break;
+		  case "ogrenciListNesne":
+			  System.out.println("Ogrenci Listesi(Nesne) Yazdiriliyor.");
+			  ArrayList<Ogrenci> ogrenciList = (ArrayList<Ogrenci>) object;
+			  for(int i=0; i<ogrenciList.size(); i++)
+			  {
+				  System.out.println(i+1 +  ". Ogrenci Adi= " +  ogrenciList.get(i).getOgrenciAdi() + " , Yasi=" + ogrenciList.get(i).getYas());
+			  }
+		    break;
+		  case "ogrenciListString":
+			  System.out.println("Ogrenci Listesi(String) Yazdiriliyor.");
+			  ArrayList<String> ogrenciList2 = (ArrayList<String>) object;
+			  for(int i=0; i<ogrenciList2.size(); i++)
+			  {
+				  System.out.println(i+1 +  ". Ogrenci= " +  ogrenciList2.get(i));
+			  }
+		    break;
+		}
+		
+
 	}
 }
 
